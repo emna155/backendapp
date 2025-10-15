201797bee88449fd1fc7fc9906bc5a3df7f14c27
emna155 <emnaha7@gmail.com>
2025-10-15 17:30:24 +0100

    clean project

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const http =require('http');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -38,4 +39,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+//mongodb+srv://emna:BRn1TNaaMUSnLozZ@cluster0.ib9mpub.mongodb.net/
+const server=http.createServer(app);
+server.listen(5000,()=>{
+  console.log('sever is running on port 5000');
+});
